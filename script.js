3376351c715ac79df3ecfbf9eca75eba5771471d
Naresh Adhe <nareshadhe01@gmail.com>
2024-09-25 17:29:44 +0530

    page2 part1 created

--- script.js
+++ script.js
@@ -70,9 +70,8 @@ tl.to(".loader", {
 })
 
 tl.from('.page1',{
-    y:"100%",
     opacity: 0,
-},"-=0.4")
+},"-=.4")
 
 tl.to('html,body',{
     overflow: 'visible',
@@ -83,8 +82,10 @@ tl.to('html,body',{
     display:"none",
 })
 
-tl.from('.navbar',{
+tl.from('.navbar svg,.navbar li',{
     opacity:0,
+    y:-30,
+    stagger:0.15,
 },"-=0.3")
 
 tl.from('#hero2 h1,#hero3 h1,#hero4 h1',{
@@ -96,10 +97,6 @@ tl.from('#hero1 h1',{
     opacity: 0,
     y:30
 },"-=.9")
-
-tl.from(".page2",{
-    opacity:0
-},"-=.2")
 }
 function cursor() {
     document.addEventListener("mousemove",(e) => {
@@ -111,6 +108,15 @@ function cursor() {
     Shery.makeMagnet(".nav-right li");
 }
 
+function sheryAnimation() {
+    Shery.imageEffect(".image-div",{
+        style:5,
+        gooey:true,
+        config:{"a":{"value":1,"range":[0,30]},"b":{"value":0.75,"range":[-1,1]},"zindex":{"value":"9996999","range":[-9999999,9999999]},"aspect":{"value":0.9500966809459568},"ignoreShapeAspect":{"value":true},"shapePosition":{"value":{"x":0,"y":0}},"shapeScale":{"value":{"x":0.5,"y":0.5}},"shapeEdgeSoftness":{"value":0,"range":[0,0.5]},"shapeRadius":{"value":0,"range":[0,2]},"currentScroll":{"value":0},"scrollLerp":{"value":0.07},"gooey":{"value":true},"infiniteGooey":{"value":true},"growSize":{"value":4,"range":[1,15]},"durationOut":{"value":1,"range":[0.1,5]},"durationIn":{"value":1.5,"range":[0.1,5]},"displaceAmount":{"value":0.5},"masker":{"value":true},"maskVal":{"value":1.2,"range":[1,5]},"scrollType":{"value":0},"geoVertex":{"range":[1,64],"value":1},"noEffectGooey":{"value":true},"onMouse":{"value":0},"noise_speed":{"value":1.3,"range":[0,10]},"metaball":{"value":0.43,"range":[0,2]},"discard_threshold":{"value":0.5,"range":[0,1]},"antialias_threshold":{"value":0,"range":[0,0.1]},"noise_height":{"value":0.5,"range":[0,2]},"noise_scale":{"value":10,"range":[0,100]}},
+    })
+}
+
 loadingAnimation();
 cursor();
 locomotiveAnimation();
+sheryAnimation();
